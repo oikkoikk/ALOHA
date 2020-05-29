f6d0fd7af614eb32fa7dca85d212ee7d284e07c3
ljjun <ljjun0702@gmail.com>
2020-05-29 16:28:23 +0900

    20200529

---
+++ 2020_Algorithm/예제구현/1535_hi.cpp
@@ -0,0 +1,48 @@
+//0-1 Knapsack
+//보석의 무게 = 체력 , 보석의 가치 = 기쁨!
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int N, W = 99; //N : 보석의 개수 , W : 배낭의 크기
+    cin >> N;
+
+    const int N_MAX = N;
+    const int W_MAX = 99;
+    int _w[N_MAX + 1], c[N_MAX + 1]; //_w[k] : k번째 보석의 무게
+                                     //c[k] : k번째 보석의 가치
+    int DP[N_MAX + 1][W_MAX + 1];
+
+    memset(_w, 0, sizeof(_w));
+    memset(c, 0, sizeof(c));
+    memset(DP, 0, sizeof(DP));
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> _w[i];
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> c[i];
+    }
+
+    for (int k = 0; k < N; k++)
+    {
+        for (int w = 1; w <= W; w++)
+        {
+            if (w >= _w[k + 1])
+            {
+                DP[k + 1][w] = max(DP[k][w], DP[k][w - _w[k + 1]] + c[k + 1]);
+            }
+            else //w-_w[k+1] <0 일 때
+            {
+                DP[k + 1][w] = DP[k][w];
+            }
+        }
+    }
+
+    cout << DP[N_MAX][W_MAX];
+}
---
+++ 2020_Algorithm/예제구현/2294_coin2.cpp
@@ -0,0 +1,45 @@
+// 동전 교환 2
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int INF = 987654321;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> coin(N + 1, 0);
+    vector<int> DP(K + 1, INF); //DP[idx]는 idx원을 만들 때 필요한 동전의 최소 개수
+                                //최소 개수를 구해야하므로 INF로 초기화!
+    DP[0] = 0;                  //첫 동전 초기화할 수 있도록 DP[0] = 0으로 초기화!
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> coin[i];
+    }
+
+    // DP 갱신
+    // 모든 동전에 대하여 확인하고 min 으로 값을 갱신한다.
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = coin[i]; j <= K; j++) //동전 각각의 금액부터 K까지!
+        {
+            DP[j] = min(DP[j], DP[j - coin[i]] + 1); // 원래 있는 대로 할지, 동전 바꿀지 결정
+        }
+    }
+
+    if (DP[K] == INF)
+        cout << -1;
+    else
+    {
+        cout << DP[K];
+    }
+
+    return 0;
+}
